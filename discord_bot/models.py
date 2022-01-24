70f6726b13508fea6438f3f4b56fbb5e1533545c
Sabyr Nurgaliyev <nurgaliyev000@gmail.com>
2022-01-24 22:33:01 +0600

    Link

--- discord_bot/models.py
+++ discord_bot/models.py
@@ -7,7 +7,7 @@ class Discord_bot(models.Model):
     image = models.ImageField(upload_to="discord_bot/images", default="C:/Users/constantion/Downloads/Telegram Desktop/photo_2022-01-13_08-56-10.jpg")
     # image = ResizedImageField (upload_to="discord_bot/images", blank=True, null=True)
     url = models.URLField(blank=True)
-
+    link = models.URLField(blank=True)
 
     def __str__(self):
         return self.title
